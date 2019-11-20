a0aa589c2dadbf6988bd47c9d0411d42d5c72053
Luis Novoa <luis.matte.novoa@gmail.com>
2019-11-20 14:39:36 -0400

    Configures all methods to return enumerators if no block is given

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -3,112 +3,164 @@
 module Enumerable
   def my_each
     instance = self
-    for i in 0...instance.length
-      yield(instance[i])
+    if block_given?
+      for i in 0...instance.length
+        yield(instance[i])
+      end
+    else
+      instance.to_enum
     end
   end
 
   def my_each_with_index
     instance = self
-    for i in 0...instance.length
-      yield(instance[i], i)
+    if block_given?
+      for i in 0...instance.length
+        yield(instance[i], i)
+      end
+    else
+      instance.to_enum
     end
   end
 
   def my_select
     instance = self
     result = []
-    instance.my_each do |e|
-      result.push(e) if yield(e)
+    if block_given?
+      instance.my_each do |e|
+        result.push(e) if yield(e)
+      end
+      result
+    else
+      instance.to_enum
     end
-    result
   end
 
   def my_all?
     instance = self
     result = true
-    instance.my_each do |e|
-      unless yield(e)
-        result = false
-        break
+    if block_given?
+      instance.my_each do |e|
+        unless yield(e)
+          result = false
+          break
+        end
       end
+      result
+    else
+      instance.to_enum
     end
-    result
   end
 
   def my_any?
     instance = self
     result = false
-    instance.my_each do |e|
-      if yield(e)
-        result = true
-        break
+    if block_given?
+      instance.my_each do |e|
+        if yield(e)
+          result = true
+          break
+        end
       end
+      result
+    else
+      instance.to_enum
     end
-    result
   end
 
   def my_none?
     instance = self
     result = true
-    instance.my_each do |e|
-      if yield(e)
-        result = false
-        break
+    if block_given?
+      instance.my_each do |e|
+        if yield(e)
+          result = false
+          break
+        end
       end
+      result
+    else
+      instance.to_enum
     end
-    result
   end
 
   def my_count
     instance = self
-    instance.length
+    if block_given?
+      instance.length
+    else
+      instance.to_enum
+    end
   end
 
   def my_map
     instance = self
     result = []
-    instance.my_each do |e|
-      result.push(yield(e))
+    if block_given?
+      instance.my_each do |e|
+        result.push(yield(e))
+      end
+      result
+    else
+      instance.to_enum
     end
-    result
   end
 
   def my_inject
     instance = self
     total = instance.shift
-    instance.each do |e|
-      total = yield(total, e)
+    if block_given?
+      instance.each do |e|
+        total = yield(total, e)
+      end
+      total
+    else
+      instance.to_enum
     end
-    total
   end
 
   def multiply_els
     instance = self
-    instance.my_inject do |sum, e|
-      sum * e
+    if block_given?
+      instance.my_inject do |sum, e|
+        sum * e
+      end
+    else
+      instance.to_enum
     end
   end
 
   def my_map_proc(&proc)
     instance = self
     result = []
-    instance.my_each do |e|
-      result.push(proc.call(e))
+    if block_given?
+      instance.my_each do |e|
+        result.push(proc.call(e))
+      end
+      result
+    else
+      instance.to_enum
     end
-    result
   end
 
   def my_map_procblock(&proc)
     instance = self
     result = []
-    instance.my_each do |e|
-      if proc.nil?
-        result.push(yield(e))
-      else
-        result.push(proc.call(e))
+    if block_given?
+      instance.my_each do |e|
+        if proc.nil?
+          result.push(yield(e))
+        else
+          result.push(proc.call(e))
+        end
       end
+      result
+    else
+      instance.to_enum
     end
-    result
   end
 end
+
+var = [1, 2, 3, 4]
+test = var.my_each
+puts test
