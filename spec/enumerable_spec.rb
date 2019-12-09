751ae62203e28d8d8928e4a5a36d11a4ee4c065f
Luis Novoa <luis.matte.novoa@gmail.com>
2019-12-09 18:07:44 -0400

    Creates test for #my_all?

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -4,17 +4,17 @@ require File.expand_path('../enumerable_methods', __dir__)
 
 RSpec.describe do
   describe '#my_each' do
-  let(:array) {[1, 2, 3, 4]}
-  let(:new_array) {[]}
-      it 'applies adds one to every element' do
-        array.my_each do |e| new_array.push(e += 1) end
-          expect(new_array).to eq([2, 3, 4, 5])
-      end
+    let(:array) {[1, 2, 3, 4]}
+    let(:new_array) {[]}
+    it 'applies adds one to every element' do
+      array.my_each do |e| new_array.push(e += 1) end
+        expect(new_array).to eq([2, 3, 4, 5])
+    end
   end
-  # 
+
   describe '#my_select' do
-  let(:arr) {[1, 2, 3, 4]}
-  let(:new_arr) {[]}
+    let(:arr) {[1, 2, 3, 4]}
+    let(:new_arr) {[]}
     it 'returns all numbers bigger than two' do
       new_arr = arr.my_select{|n| n > 2}
       expect(new_arr).to eq([3, 4])
@@ -24,5 +24,11 @@ RSpec.describe do
       expect(new_arr.class).to eq(Enumerator)
     end
   end
-  # 
+
+  describe '#my_all?' do
+    let(:arr) {[1, 2, 3, 4]}
+    it 'returns true for the array of Integers' do
+      expect(arr.my_all?(Integer)).to eq(true)
+    end
+  end
 end
