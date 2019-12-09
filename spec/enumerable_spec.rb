24f8f5f513fc3f932b502d66b6170fe321dfa769
RoodzFernando <fleurantrfernando@gmail.com>
2019-12-09 16:58:22 -0500

    Finished the test for my_select enum

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -3,12 +3,26 @@
 require File.expand_path('../enumerable_methods', __dir__)
 
 RSpec.describe do
-  describe 'my_each' do
-    let(:array) {[1, 2, 3, 4]}
-    let(:new_array) {[]}
+  describe '#my_each' do
+  let(:array) {[1, 2, 3, 4]}
+  let(:new_array) {[]}
       it 'applies adds one to every element' do
         array.my_each do |e| new_array.push(e += 1) end
-      expect(new_array).to eq([2, 3, 4, 5])
+          expect(new_array).to eq([2, 3, 4, 5])
+      end
+  end
+  # 
+  describe '#my_select' do
+  let(:arr) {[1, 2, 3, 4]}
+  let(:new_arr) {[]}
+    it 'returns all numbers bigger than two' do
+      new_arr = arr.my_select{|n| n > 2}
+      expect(new_arr).to eq([3, 4])
+    end
+    it 'returns en enumerable when no block is passed'do
+      new_arr = arr.my_select
+      expect(new_arr.class).to eq(Enumerator)
     end
   end
+  # 
 end
