3248de78ccb3c4c99f2d668eaa2a50033e372feb
Luis Novoa <luis.matte.novoa@gmail.com>
2019-12-09 18:18:47 -0400

    Fixes rubocop errors

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -4,50 +4,53 @@ require File.expand_path('../enumerable_methods', __dir__)
 
 RSpec.describe do
   describe '#my_each' do
-    let(:array) {[1, 2, 3, 4]}
-    let(:new_array) {[]}
+    let(:array) { [1, 2, 3, 4] }
+    let(:new_array) { [] }
     it 'applies adds one to every element' do
-      array.my_each do |e| new_array.push(e += 1) end
-        expect(new_array).to eq([2, 3, 4, 5])
+      array.my_each do |e|
+        e += 1
+        new_array.push(e)
+      end
+      expect(new_array).to eq([2, 3, 4, 5])
     end
   end
 
   describe '#my_select' do
-    let(:arr) {[1, 2, 3, 4]}
-    let(:new_arr) {[]}
+    let(:arr) { [1, 2, 3, 4] }
+    let(:new_arr) { [] }
     it 'returns all numbers bigger than two' do
-      new_arr = arr.my_select{|n| n > 2}
+      new_arr = arr.my_select { |n| n > 2 }
       expect(new_arr).to eq([3, 4])
     end
-    it 'returns en enumerable when no block is passed'do
+    it 'returns en enumerable when no block is passed' do
       new_arr = arr.my_select
       expect(new_arr.class).to eq(Enumerator)
     end
   end
 
   describe '#my_all?' do
-    let(:arr) {[1, 2, 3, 4]}
+    let(:arr) { [1, 2, 3, 4] }
     it 'returns true for the array of Integers' do
       expect(arr.my_all?(Integer)).to eq(true)
     end
   end
 
   describe '#my_any?' do
-    let(:arr) {[1, 2, 3, 4]}
+    let(:arr) { [1, 2, 3, 4] }
     it 'returns false because there\'s no string' do
       expect(arr.my_any?(String)).to eq(false)
     end
   end
 
   describe '#my_none?' do
-    let(:arr) {[1, 2, 3, 4]}
+    let(:arr) { [1, 2, 3, 4] }
     it 'returns true because there\'s no arrays' do
       expect(arr.my_none?(Array)).to eq(true)
     end
   end
 
   describe '#my_inject' do
-    let(:arr) {[1, 2, 3, 4]}
+    let(:arr) { [1, 2, 3, 4] }
     it 'returns the sum of the array' do
       expect(arr.my_inject(:+)).to eq(10)
     end
